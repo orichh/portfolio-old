56bfed5830ef5dfe250b91302d1a92e91d2bdb24
richard <orichh@gmail.com>
2021-08-10 21:09:56 -0400

    add portfolio app and bootstrap template docs

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('reviewsite/', include('reviewsite.urls')),
+    path('portfolio/', include('portfolio.urls')),
     path('admin/', admin.site.urls),
 ]

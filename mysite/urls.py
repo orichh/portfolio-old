bbf35d4c1eb3a0a46e2c7badd7b19ee81583e3f5
root <root@DESKTOP-S9OUIFE.localdomain>
2021-08-14 12:29:28 -0400

    added procfile and requirements

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('portfolio.urls')),
     path('reviewsite/', include('reviewsite.urls')),
     path('portfolio/', include('portfolio.urls')),
     path('admin/', admin.site.urls),
